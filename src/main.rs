69508505345f913f132ef1cfcb97e8ecf1c59306
walther chen <walther.chen@gmail.com>
2026-04-15 11:29:33 +0700

    rust: refactor main for clarity

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,59 @@ use std::io;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::fs::File;
 
-use clap::{Arg, App, crate_authors, crate_version};
+use clap::{Arg, App, ArgMatches, crate_authors, crate_version};
 use csv::{QuoteStyle, WriterBuilder};
 use json::JsonValue;
 
 const KEYS_ENV_VAR: &'static str = "KEYS";
 
 fn main() {
-    let app = App::new("json2csv")
+    let app = get_opts();
+
+    let reader: Box<dyn Read> = match app.value_of("file_in") {
+        Some(path) => Box::new(File::open(path).unwrap()),
+        _ => Box::new(io::stdin()),
+    };
+
+    let writer: Box<dyn Write> = match app.value_of("file_out") {
+        Some(path) => Box::new(File::open(path).unwrap()),
+        _ => Box::new(io::stdout()),
+    };
+
+    let keys: Vec<_> = match app.values_of("keys") {
+        Some(keys) => keys.map(|k|k.to_owned()).collect(),
+        _ => {
+            let env_keys = env::var(KEYS_ENV_VAR).unwrap();
+            env_keys.split(',').map(|k| k.to_owned()).collect()
+        }
+    };
+
+    // Core logic
+    let reader = BufReader::new(reader);
+
+    let mut csv_writer = WriterBuilder::new()
+        .escape(b'\\')
+        .quote(b'\'')
+        .quote_style(QuoteStyle::Necessary)
+        .from_writer(writer);
+
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parsed = json::parse(&line).unwrap();
+        if let JsonValue::Object(obj) = parsed {
+            for key in &keys {
+                let k = obj.get(key)
+                    .map(|k| k.as_str().unwrap())
+                    .unwrap_or("");
+                csv_writer.write_field(k).unwrap();
+            }
+        }
+    }
+}
+
+fn get_opts() -> ArgMatches<'static> {
+    App::new("json2csv")
         .version(crate_version!())
         .author(crate_authors!())
         .about("converts lines of json to csv")
@@ -35,54 +80,5 @@ fn main() {
              .multiple(true)
              .number_of_values(1)
              .help("for each row, filter by keys. Takes multiple values, one per -k"))
-        .get_matches();
-
-    let reader: Box<dyn Read> = match app.value_of("file_in") {
-        Some(path) => Box::new(File::open(path).unwrap()),
-        _ => Box::new(io::stdin()),
-    };
-
-    let writer: Box<dyn Write> = match app.value_of("file_out") {
-        Some(path) => Box::new(File::open(path).unwrap()),
-        _ => Box::new(io::stdout()),
-    };
-
-    let keys: Vec<_> = match app.values_of("keys") {
-        Some(keys) => keys.map(|k|k.to_owned()).collect(),
-        _ => {
-            let env_keys = env::var(KEYS_ENV_VAR).unwrap();
-            env_keys.split(',').map(|k| k.to_owned()).collect()
-        }
-    };
-
-    json_to_csv(reader, writer, &keys);
-}
-
-pub fn json_to_csv<R: Read, W: Write>(
-    reader: R,
-    writer: W,
-    keys: &[String],
-    )
-{
-    let reader = BufReader::new(reader);
-
-    let mut csv_writer = WriterBuilder::new()
-        .escape(b'\\')
-        .quote(b'\'')
-        .quote_style(QuoteStyle::Necessary)
-        .from_writer(writer);
-
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let parsed = json::parse(&line).unwrap();
-        if let JsonValue::Object(obj) = parsed {
-            for key in keys {
-                let k = obj.get(key)
-                    .map(|k| k.as_str().unwrap())
-                    .unwrap_or("");
-                csv_writer.write_field(k).unwrap();
-            }
-        }
-    }
+        .get_matches()
 }
